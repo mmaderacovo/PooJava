eb4c87f957a35f7bee12b532e77160a72dd8b6c5
Angelica <mmaderacovo@gmail.com>
2022-12-21 09:28:30 -0500

    Java Poo

---
+++ src/Doctor.java
@@ -0,0 +1,17 @@
+public class Doctor {
+    int id;
+    String name;
+    String speciality;
+
+    Doctor(){
+        System.out.println("Construyendo el objeto Doctor");
+    }
+
+    Doctor (String name){
+        System.out.println("El nombre del doctor asignado es: "+ name);
+    }
+    public void showName(){
+        System.out.println(name);
+    }
+
+}
