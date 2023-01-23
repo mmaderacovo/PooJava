0fd4b370a0b75db2b61e86cdc3b0f6d8a833efa0
Angelica <mmaderacovo@gmail.com>
2023-01-23 14:42:57 -0500

    Creando la clase paciente

--- src/Doctor.java
+++ src/Doctor.java
@@ -5,11 +5,14 @@ public class Doctor {
 
     Doctor(){
         System.out.println("Construyendo el objeto Doctor");
-        id++;
+
     }
 
-    Doctor(String name){
+    Doctor(String name,String speciality){
         System.out.println("El nombre del doctor asignado es: "+ name);
+        id++;
+        this.name=name;
+        this.speciality=speciality;
     }
     public void showName(){
         System.out.println(name);
---
+++ src/Patient.java
@@ -0,0 +1,10 @@
+public class Patient {
+    String name, email, address, birthday, phoneNumber, typeOfBlood;
+    double weight,height;
+
+    Patient(String name, String email){
+        this.name=name;
+        this.email=email;
+    }
+
+}
